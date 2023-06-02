2d2a78679b0a37d68d664003b37773726ceb5721
feliposz <feliposz@gmail.com>
2023-06-01 23:31:29 -0300

    Updating 2023-06-01

---
+++ TypeScript/generate-fibonacci-sequence.ts
@@ -0,0 +1,15 @@
+function* fibGenerator(): Generator<number, any, number> {
+    yield 0
+    let a = 0
+    let b = 1
+    while (true) {
+        [a, b] = [b, a + b]
+        yield a
+    }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
