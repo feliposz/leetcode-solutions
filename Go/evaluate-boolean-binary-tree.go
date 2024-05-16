e06a8fa7002377534e3fa5fdef0e38f0768b4877
feliposz <feliposz@gmail.com>
2024-05-16 14:24:39 -0300

    Updated 2024-05-16

---
+++ Go/evaluate-boolean-binary-tree.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func evaluateTree(root *TreeNode) bool {
+	switch root.Val {
+	case 0:
+		return false
+	case 1:
+		return true
+	case 2:
+		return evaluateTree(root.Left) || evaluateTree(root.Right)
+	case 3:
+		return evaluateTree(root.Left) && evaluateTree(root.Right)
+	}
+    panic("not implemented")
+    return false
+}
