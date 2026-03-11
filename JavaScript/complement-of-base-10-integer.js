83da54d7e124487d7d124024d9265bd5b85d5684
feliposz <feliposz@gmail.com>
2026-03-11 19:18:59 -0300

    Updated 2026-03-11

---
+++ JavaScript/complement-of-base-10-integer.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var bitwiseComplement = function(n) {
+    if (n === 0) {
+        return 1
+    }
+    let mask = n
+    mask |= mask >> 1
+    mask |= mask >> 2
+    mask |= mask >> 4
+    mask |= mask >> 8
+    mask |= mask >> 16
+    return mask ^ n
+};
